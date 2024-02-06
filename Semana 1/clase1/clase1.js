edbdb26a8646fd183cd4544c1b7d84eb3dc01fda
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-02-06 19:47:52 -0300

    Primer ejercicio, alert-confirm-prompt

--- Semana 1/clase1/clase1.js
+++ Semana 1/clase1/clase1.js
@@ -1,31 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 1                                 */
 /* -------------------------------------------------------------------------- */
@@ -47,7 +19,6 @@
 //       return nombre;
 //   }
 
-
   /* -------------------------------------------------------------------------- */
   /*                          CONSIGNA MESA DE TRABAJO                          */
   /* -------------------------------------------------------------------------- */
@@ -55,3 +26,33 @@
   // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
   // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
 
+  function iniciarJuego() {
+    let nombre = '';
+
+    alert("Bienvenido al piedra papel o tijera de Frontend II.");
+  
+    // Solicitar nombre válido (más de 3 caracteres y no vacío)
+    do {
+        nombre = prompt("Ingrese su nombre por favor:");
+        if (nombre === null) {
+            return null; // Si cancela, sale de la función. Devuelve NULL.
+        }
+        if (nombre.length < 3) {
+            alert("El nombre debe tener al menos 3 caracteres. Por favor, inténtelo de nuevo.");
+        }
+    } while (nombre.length < 3);
+  
+    // Convertir nombre a mayúsculas
+    nombre = nombre.toUpperCase();
+    alert("Gracias por jugar, " + nombre + ". ¡Mucha suerte!");
+  
+    // Mostrar nombre por consola
+    console.log("****************************");
+    console.log("El jugador es:");
+    console.log(nombre);
+    console.log("****************************");
+  
+    return nombre;
+  }
+
+  iniciarJuego();
